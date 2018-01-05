b3e1a2167d3f1fd841d9f340190b632aa7306662
Horst JENS <horstjens@gmail.com>
2018-01-05 09:50:27 +0100

    multi level dungeon (3d)

--- dungeongame.py
+++ dungeongame.py
@@ -1,24 +1,58 @@
 # max im Dungeon
 # dieses Spiel braucht Python3
-# b = banana
-# h = healing
-# # = mauer 
+helptext = """
+b = banana
+h = healing
+# = mauer 
+d = door
+k = key
+< = stiege rauf
+> = stiege runter
+@ = Kevin the hero
+"""
 kevin = "@"
 keller1 = """
 ########################################
-#......................................#
-#......................................#
-#......................................#
-#......................................#
-#......................................#
+#..............................##......#
+#.............................#.#......#
+#..>.........................#..#......#
+#...............................#......#
+#...............................#......#
 #......................................#
 ########################################
 """
-keller = []
-for line in keller1.splitlines():
-    keller.append(list(line))
-   
+keller2 = """
+##############################################
+#..........k.......................##........#
+#.................................#..#.......#
+#..<>............###d####............#.......#
+#................#......#...........#........#
+#................#......#..........####......#
+#................#......#....................#
+##############################################
+"""
+keller3 = """
+#################################################
+#.....................................###.......#
+#.......................................#.......#
+#...<..................................##.......#
+#.......................................#.......#
+#.....................................###.......#
+#...............................................#
+#################################################
+"""
 
+
+
+keller = []
+for k in [keller1, keller2, keller3]:
+    ebene = []
+    for line in k.splitlines():
+        ebene.append(list(line))
+    keller.append(ebene)
+        
+   
+kz = 0
 ky = 2
 kx = 1
 hp = 100
@@ -26,12 +60,12 @@ hunger = 0
 turn = 0
 food = 5
 heal = 1
-
+keys = 0
 
 
 while hp > 0 and hunger < 100:
     # ---- grafik anzeigen ----
-    for y, line in enumerate(keller):
+    for y, line in enumerate(keller[kz]):
         for x, char in enumerate(line):
             if y == ky and x == kx:
                 print(kevin, end="")
@@ -44,18 +78,23 @@ while hp > 0 and hunger < 100:
     if turn % 10 == 0:
         hunger += 1  # alle 10 turns einen hunger dazu tun
     # ---- pick up stuff ----
-    if keller[ky][kx] == "b":
+    if keller[kz][ky][kx] == "b":
         print("Du findest eine Banane und nimmst sie mit")
         food += 1
-        keller[ky][kx] = "."
-    if keller[ky][kx] == "h":
+        keller[kz][ky][kx] = "."
+    if keller[kz][ky][kx] == "h":
         print("du findest ein Medpack und nimmst es mit")
         heal += 1
-        keller[ky][kx] = "."
+        keller[kz][ky][kx] = "."
+    # ---- stairs ----
+    if keller[kz][ky][kx] == ">":
+        special += " *stair down* "
+    if keller[kz][ky][kx] == "<":
+        special += " *stair up* "
     # ---- status ----
     if hunger > 50:
-        special = "*hungry*"
-    status = "{} hp: {} hu: {} food: {} med: {} #: {} ".format(
+        special += " *hungry* "
+    status = "{}\nhp: {} hu: {} food: {} med: {} #: {} ".format(
              special, hp, hunger, food, heal, turn)
     # ---- keyboard control -----
     command = input(status+">>>")
@@ -69,14 +108,36 @@ while hp > 0 and hunger < 100:
     if command == "s":
         dy = 1
     # --- wall check ---
-    if keller[ky+dy][kx+dx] == "#":
+    if keller[kz][ky+dy][kx+dx] == "#":
         print("aua!!")
         hp -= 1
         dx, dy = 0, 0
+    # ---- door check ----
+    if keller[kz][ky+dy][kx+dx] == "d":
+        if keys <= 0:
+            print("aua, benutz einen Schlüssel (k)")
+            hp -= 1
+            dx, dy = 0, 0
+        else:
+            print("Du öffnest die Türe und verbrauchst einen Schlüssel")
+            keys -= 1
+            keller[kz][ky+dy][kx+dx] = "."
+            
     # --- movement -----
     ky += dy
     kx += dx
-    
+    # --- climb stair ----
+    if command == "c" or command == "climb":
+        if keller[kz][ky][kx] == ">":
+            print("you climb down from level {} to level {}".format(
+                   kz, kz+1))
+            kz += 1
+        elif keller[kz][ky][kx] == "<":
+            print("you climb up from level {} to level {}".format(
+                    kz, kz-1))
+            kz -= 1
+        else:
+            print("you need to find a stair (< or >) to climb")
     if command == "quit":
         print("bye-bye")
         break
@@ -84,6 +145,9 @@ while hp > 0 and hunger < 100:
         print("Gar nicht wahr!")
         print("mit dir spielt Kevin nicht mehr!")
         break
+    if command == "?" or command == "help":
+        print(helptext)
+        input("press Enter")
     if command == "e":
         if food <= 0:
             print("du hast gar keine Banane!")
