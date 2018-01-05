f621578956c039241560960d657eab63d2ebd217
Horst JENS <horstjens@gmail.com>
2018-01-05 09:48:15 +0100

    2-dimensional game

--- dungeongame.py
+++ dungeongame.py
@@ -2,38 +2,56 @@
 # dieses Spiel braucht Python3
 # b = banana
 # h = healing
+# # = mauer 
 kevin = "@"
-keller = list("...b....b....b....h...h...h...b..")
+keller1 = """
+########################################
+#......................................#
+#......................................#
+#......................................#
+#......................................#
+#......................................#
+#......................................#
+########################################
+"""
+keller = []
+for line in keller1.splitlines():
+    keller.append(list(line))
+   
 
-kx = 0
+ky = 2
+kx = 1
 hp = 100
 hunger = 0
 turn = 0
 food = 5
 heal = 1
 
+
+
 while hp > 0 and hunger < 100:
     # ---- grafik anzeigen ----
-    for x, tile in enumerate(keller):
-        if x == kx:
-            print(kevin, end="")
-        else:
-            print(tile, end="")
-    print() # end of line
+    for y, line in enumerate(keller):
+        for x, char in enumerate(line):
+            if y == ky and x == kx:
+                print(kevin, end="")
+            else:
+                print(char, end="")
+        print() # end of line
     # ---- progress ---
     special = ""
     turn += 1
     if turn % 10 == 0:
         hunger += 1  # alle 10 turns einen hunger dazu tun
     # ---- pick up stuff ----
-    if keller[kx] == "b":
+    if keller[ky][kx] == "b":
         print("Du findest eine Banane und nimmst sie mit")
         food += 1
-        keller[kx] = "."
-    if keller[kx] == "h":
+        keller[ky][kx] = "."
+    if keller[ky][kx] == "h":
         print("du findest ein Medpack und nimmst es mit")
         heal += 1
-        keller[kx] = "."
+        keller[ky][kx] = "."
     # ---- status ----
     if hunger > 50:
         special = "*hungry*"
@@ -41,10 +59,24 @@ while hp > 0 and hunger < 100:
              special, hp, hunger, food, heal, turn)
     # ---- keyboard control -----
     command = input(status+">>>")
+    dx, dy = 0, 0
     if command == "a":
-        kx -= 1
+        dx = -1
     if command == "d":
-        kx += 1
+        dx = 1
+    if command == "w":
+        dy = -1
+    if command == "s":
+        dy = 1
+    # --- wall check ---
+    if keller[ky+dy][kx+dx] == "#":
+        print("aua!!")
+        hp -= 1
+        dx, dy = 0, 0
+    # --- movement -----
+    ky += dy
+    kx += dx
+    
     if command == "quit":
         print("bye-bye")
         break
@@ -66,3 +98,4 @@ print("Game Over")
     
     
     
+
